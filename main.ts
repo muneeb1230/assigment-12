9f64aef1a59a28b542eed253d223f5e415c973c3
Muneeb Malik <144794302+muneeb1230@users.noreply.github.com>
2024-05-09 02:07:17 +0500

    Update main.ts
    comment out

--- main.ts
+++ main.ts
@@ -1,46 +1,46 @@
-// // create two variables and add them//
-// // let num1: number = 2
-// // let num2: number = 10
-// // let sum: number = num1 + num2
-// // console.log(sum)
+// create two variables and add them//
+// let num1: number = 2
+// let num2: number = 10
+// let sum: number = num1 + num2
+// console.log(sum)
 
-// // // and multiplication ///
-// // sum = num1* num2
-// // console.log(sum)
+// // and multiplication ///
+// sum = num1* num2
+// console.log(sum)
 
 
-// // let num3: number = 0
-// // num3 = 5
-// // console.log(num3* 5)
-// // console.log(num3 + 20)
-// import inquirer from "inquirer"
-// // calculator //
-// const answer = await inquirer.prompt([
-//     {
-//       message: "Enter first name",
-//       name: "first",
-//       type: "number"  
-//     },
-//     {
-//         message: "enter second number",
-//         name: "second",
-//         type: "number"
-//     },
-//     {
-//         message: "select operators",
-//         name: "operator",
-//         type: "list",
-//         choices: ["Addition", "substraction", "Multiplication", "Duivision"]
-//     }
-// ])
-// if(answer.operator === "Addition"){
-//     console.log(answer.first + answer.second)
-// }else if(answer.operator === "substraction"){
-//     console.log(answer.first - answer.second)
-// }else if(answer.operator === "Multiplication"){
-//     console.log(answer.first * answer.second)
-// }else if(answer.operator === "Division "){
-//     console.log(answer.first/ answer.second)
-// }else{
-//     console.log("plz select operators valid")
-// }
+// let num3: number = 0
+// num3 = 5
+// console.log(num3* 5)
+// console.log(num3 + 20)
+import inquirer from "inquirer"
+// calculator //
+const answer = await inquirer.prompt([
+    {
+      message: "Enter first name",
+      name: "first",
+      type: "number"  
+    },
+    {
+        message: "enter second number",
+        name: "second",
+        type: "number"
+    },
+    {
+        message: "select operators",
+        name: "operator",
+        type: "list",
+        choices: ["Addition", "substraction", "Multiplication", "Duivision"]
+    }
+])
+if(answer.operator === "Addition"){
+    console.log(answer.first + answer.second)
+}else if(answer.operator === "substraction"){
+    console.log(answer.first - answer.second)
+}else if(answer.operator === "Multiplication"){
+    console.log(answer.first * answer.second)
+}else if(answer.operator === "Division "){
+    console.log(answer.first/ answer.second)
+}else{
+    console.log("plz select operators valid")
+}
