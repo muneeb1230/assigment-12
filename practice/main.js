223c3148fb647a20fd3fb1fa7e5a37e82c568ae7
Muneeb Malik <144794302+muneeb1230@users.noreply.github.com>
2024-05-10 00:09:43 +0500

    new filr

---
+++ practice/main.js
@@ -0,0 +1,36 @@
+"use strict";
+let age = 20;
+let message = age >= 18 ? "you get a licence" : "you dont get licence";
+console.log(message);
+// switch case statements//----====
+let day = 5;
+switch (day) {
+    case 1:
+        console.log("monday");
+        break;
+    case 2:
+        console.log("tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("friday");
+        break;
+    case 6:
+        console.log("saturday");
+        break;
+    case 7:
+        console.log("sunday");
+        break;
+    default:
+        console.log("there is no day ");
+        break;
+}
+// 3. Create a variable and make thier value dynamic by using ternary operator (?).  You can create any dynamic value or program//
+let admin = false;
+let user = admin ? "Adminstrator" : "regular user";
+console.log(`userRole ${user}`);
